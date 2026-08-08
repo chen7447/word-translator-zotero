dd71d10328545206c7db3d63379b65c25f20487a
chen7447 <chenzewei0830@gmail.com>
2026-08-08 23:31:59 +0800

    chore: sync v5.1.10 source

--- src/wordtranslator/prefs.js
+++ src/wordtranslator/prefs.js
@@ -1,5 +1,5 @@
 // Word Translator for Zotero 默认偏好
-pref("extensions.zotero.wordtranslator.version", "4.5.0");
+pref("extensions.zotero.wordtranslator.version", "5.0.0b1");
 pref("extensions.zotero.wordtranslator.contextMenuLabel", "添加单词并翻译");
 pref("extensions.zotero.wordtranslator.enabled", true);
 pref("extensions.zotero.wordtranslator.autoTranslate", false);
