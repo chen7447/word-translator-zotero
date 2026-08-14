54aa9842c69988b451488789cd948a761afb52b5
chen7447 <chen7447@users.noreply.github.com>
2026-08-14 21:57:13 +0800

    chore: 版本号 6.7.0b3 -> 6.7.0b4（修复翻译结果卡" 翻译中\）

--- build/addon/prefs.js
+++ build/addon/prefs.js
@@ -1,5 +1,5 @@
 // Word Translator for Zotero 默认偏好
-pref("extensions.zotero.wordtranslator.version", "6.7.0b3");
+pref("extensions.zotero.wordtranslator.version", "6.7.0b4");
 pref("extensions.zotero.wordtranslator.contextMenuLabel", "添加单词并翻译");
 pref("extensions.zotero.wordtranslator.enabled", true);
 pref("extensions.zotero.wordtranslator.autoTranslate", false);
